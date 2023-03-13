c6be3cc371a01b6cabd895341620e80b5299363a
Mahfod <mahfod.dev@gmail.com>
2023-03-13 20:18:59 +0100

    data homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
-import { Component, createSignal } from 'solid-js';
-import { Router } from 'solid-app-router';
+import { Component, createSignal, createResource } from 'solid-js';
+import { Routes, Route } from 'solid-app-router';
 import { Header } from './components/Header';
 import { Product } from './interface/product';
+import { HomePage } from './page/HomePage';
 
 const App: Component = () => {
 	const [search, setSearch] = createSignal('');
-  const [cart, setCart] = createSignal<Product[]>([]);
+	const [cart, setCart] = createSignal<Product[]>([]);
+
+	const [products] = createResource<Product[]>(
+		() =>
+			fetch('https://fakestoreapi.com/products').then((res) =>
+				res.json()
+			),
+		{
+			initialValue: [],
+		}
+	);
 
 	return (
-		
-			<Router>
-				<Header search={search} onClearCart={()=>setCart([])} onSetSearch={(str)=> setSearch(str)} cart={cart} />
-			</Router>
-	
+		<>
+			<Header
+				search={search}
+				onClearCart={() => setCart([])}
+				onSetSearch={(str) => setSearch(str)}
+				cart={cart}
+			/>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<HomePage
+							products={products}
+							onAddToCart={(p) => setCart([...cart(), p])}
+							search={search}
+						/>
+					}
+				/>
+			</Routes>
+		</>
 	);
 };
 
